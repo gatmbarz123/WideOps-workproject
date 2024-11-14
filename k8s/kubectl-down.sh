a3f019f710cefabbb0ad08c89b83851104931040
gatmbarz123 <bar30301@gmail.com>
2024-11-14 13:00:55 +0200

    Fix K8S

--- k8s/kubectl-down.sh
+++ k8s/kubectl-down.sh
@@ -3,7 +3,6 @@
 kubectl delete -f wordpress-secrets.yml
 kubectl delete -f ingress.yml
 kubectl delete -f horizontal.yml
-kubectl delete -f storage-class.yml
 kubectl delete -f wordpress-pvc.yml
 kubectl delete -f wordpress.yml
 
--- k8s/kubectl-up.sh
+++ k8s/kubectl-up.sh
@@ -3,7 +3,6 @@
 kubectl apply -f wordpress-secrets.yml
 kubectl apply -f ingress.yml
 kubectl apply -f horizontal.yml
-kubectl apply -f storage-class.yml
 kubectl apply -f wordpress-pvc.yml
 kubectl apply -f wordpress.yml
 
